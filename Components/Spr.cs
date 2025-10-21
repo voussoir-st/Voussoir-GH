57fa5480e97dca145045e6eac14ec33c04cc1395
STBIM <stbimfaup@gmail.com>
2025-10-21 17:54:53 +0100

    2025-10-21

--- Components/Spr.cs
+++ Components/Spr.cs
@@ -23,6 +23,29 @@ namespace VoussoirPlugin03
 
     namespace Components
     {
+        public static class Utilsb
+        {
+            public static void OrientArcs(List<Curve> arcs)
+            {
+                if (arcs == null || arcs.Count != 2)
+                    return;
+
+                Point3d arc0Start = arcs[0].PointAtStart;
+                Point3d arc0End = arcs[0].PointAtEnd;
+                Point3d arc1Start = arcs[1].PointAtStart;
+                Point3d arc1End = arcs[1].PointAtEnd;
+
+                double dStart = arc0Start.DistanceTo(arc1Start);
+                double dEnd = arc0End.DistanceTo(arc1End);
+                double dCrossStartEnd = arc0Start.DistanceTo(arc1End);
+                double dCrossEndStart = arc0End.DistanceTo(arc1Start);
+
+                if (dStart + dEnd > dCrossStartEnd + dCrossEndStart)
+                {
+                    arcs[1].Reverse();
+                }
+            }
+        }
         public class VoussoirCreate : GH_Component
         {
             public VoussoirCreate()
@@ -57,7 +80,7 @@ namespace VoussoirPlugin03
             {
                 pManager.AddBrepParameter("Springers", "S", "Finished Springers", GH_ParamAccess.list);
                 pManager.AddBrepParameter("Voussoirs", "V", "Non transformed voussoirs", GH_ParamAccess.tree);
-                //pManager.AddCurveParameter("Log", "L", "All messages generated during execution", GH_ParamAccess.tree);
+                pManager.AddVectorParameter("Log", "L", "All messages generated during execution", GH_ParamAccess.list);
             }
 
             protected override void SolveInstance(IGH_DataAccess DA)
@@ -78,6 +101,44 @@ namespace VoussoirPlugin03
                 if (!DA.GetDataList(2, tPlanes)) return;
                 if (!DA.GetData(3, ref spWidth)) return;
 
+                //=========================================
+                // springer line intersection points
+                //=========================================
+                List<Point3d> splPtsA = new List<Point3d>();
+                List<Point3d> splPtsB = new List<Point3d>();
+                Utilsb.OrientArcs(spLine);
+                List<Vector3d> sprVector = new List<Vector3d>();
+
+                foreach (Plane pl in tPlanes)
+                {
+                    double t; // parameter along the line where intersection occurs
+                    Line line = new Line(spLine[0].PointAtStart, spLine[0].PointAtEnd);
+                    
+                        if (Rhino.Geometry.Intersect.Intersection.LinePlane(line, pl, out t))
+                        {
+                            // Compute the 3D point at parameter t
+                            Point3d pt = line.PointAt(t);
+                            splPtsA.Add(pt);
+                        }
+
+                    double p; // parameter along the line where intersection occurs
+                    Line lineB = new Line(spLine[1].PointAtStart, spLine[1].PointAtEnd);
+
+                    if (Rhino.Geometry.Intersect.Intersection.LinePlane(lineB, pl, out p))
+                    {
+                        // Compute the 3D point at parameter t
+                        Point3d pt = lineB.PointAt(p);
+                        splPtsB.Add(pt);
+                    }
+                }
+                for (int i = 0; i < splPtsA.Count; i++)
+                {
+                    Line spl = new Line(splPtsA[i], splPtsB[i]);
+                    Vector3d splV = spl.PointAt(1) - spl.PointAt(0);
+                    splV.Unitize();
+                    sprVector.Add(splV);
+                }
+                DA.SetDataList(2, sprVector);
                 //=========================================
                 // Remap voussoirs tree: {A;B} → {B}(A) (Path Mapper style)
                 //=========================================
@@ -134,6 +195,7 @@ namespace VoussoirPlugin03
                 //=========================================
                 // Create Springers
                 //=========================================
+                List<Brep> springers = new List<Brep>();
 
                 foreach (Curve line in spLine)
                 {
@@ -169,7 +231,51 @@ namespace VoussoirPlugin03
                                 // Store the original voussoir in its respective branch
                                 intersectedVoussoirs.Append(vouss, branchPath);
                             }
-                        } 
+                        }
+
+                        GH_Structure<GH_Number> secondZValues = new GH_Structure<GH_Number>();
+
+                        for (int i = 0; i < intersectedVoussoirs.Branches.Count; i++)
+                        {
+                            var b = intersectedVoussoirs.Branches[i];
+                            GH_Path path = intersectedVoussoirs.Paths[i];
+
+                            // Lista temporária para armazenar todos os valores Z deste ramo
+                            List<double> zValues = new List<double>();
+
+                            foreach (var ghBrep in b)
+                            {
+                                Brep brep = ghBrep.Value;
+
+                                // Iterar pelos vértices de cada Brep
+                                foreach (BrepVertex v in brep.Vertices)
+                                {
+                                    zValues.Add(v.Location.Z);
+                                }
+                            }
+
+                            // Ordenar os valores de Z do maior para o menor
+                            zValues.Sort();
+                            zValues.Reverse();
+
+                            // Se existirem pelo menos dois valores, guarda o de índice 1
+                            if (zValues.Count > 1)
+                            {
+                                secondZValues.Append(new GH_Number(zValues[1]), path);
+                            }
+                            else if (zValues.Count == 1)
+                            {
+                                // Caso especial: só há um valor, guarda esse
+                                secondZValues.Append(new GH_Number(zValues[0]), path);
+                            }
+                            else
+                            {
+                                // Caso o ramo esteja vazio, podes guardar null ou ignorar
+                                secondZValues.Append(null, path);
+                            }
+                        }
+
+                        //DA.SetDataTree(2, secondZValues);
                     }
                 }
                 //DA.SetDataList(0, walls);
